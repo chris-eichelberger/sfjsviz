e49beaf76918fb98fb74914d5580b3be12cf22de
Chris Eichelberger <cne1x@ccri.com>
2015-06-08 11:19:33 -0400

    Separate rotation.

--- viz.js
+++ viz.js
@@ -4,10 +4,25 @@ var view_ratio = view_width / view_height;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 90, view_ratio, 0.1, 1000 );
+camera.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( view_width, view_height );
 document.body.appendChild( renderer.domElement );
+
+controls = new THREE.OrbitControls( camera );
+controls.target.set(0, 0, 0);
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+controls.keys = [ 65, 83, 68 ];
+controls.addEventListener( 'change', render );
+
+
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -53,7 +68,20 @@ function isSelected(x, y, z) {
 
 function toggle_rotation() {
   rotate = !rotate;
+  if (rotate) $( "#btn_rotate" )[0].value = "stop rotating";
+  else $( "#btn_rotate" )[0].value = "rotate";
 }
+
+function toggle_rotation_axis(axis) {
+  rotate[axis] = !rotate[axis];
+  var btnid = "#btn_rotate_" + axis;
+  if (rotate[axis]) $( btnid )[0].value = "stop rotating " + axis;
+  else $( btnid )[0].value = "rotate " + axis;
+}
+
+function toggle_rotation_x() { toggle_rotation_axis("x"); }
+function toggle_rotation_y() { toggle_rotation_axis("y"); }
+function toggle_rotation_z() { toggle_rotation_axis("z"); }
 
 var sfc_group = new THREE.Group();
 
@@ -126,30 +154,14 @@ for (var i=1; i < sfc.nodes.length; i++) {
 scene.add(sfc_group);
 
 
-// create the SFC shape
-var lines_geometry = new THREE.Geometry();
-for (var i=0; i < sfc.nodes.length; i++) {
-  var point = getPoint(i);
-  var px = point[0] * dx - off_x;
-  var py = point[1] * dy - off_y;
-  var pz = point[2] * dz - off_z;
-
-  lines_geometry.vertices.push(
-    new THREE.Vector3(px, py, pz)
-  );
-}
-var lines_material = new THREE.MeshBasicMaterial( { color: 0xff8040 } );
-var lines = new THREE.Line( lines_geometry, lines_material );
-//scene.add( lines );
-
-camera.position.z = 5;
-
 function render() {
   requestAnimationFrame( render );
-  if (rotate) {
-    sfc_group.rotation.x += 0.001;
-    sfc_group.rotation.y += 0.002;
-  }
+
+  if (rotate["x"]) { sfc_group.rotation.x += 0.001; }
+  if (rotate["y"]) { sfc_group.rotation.y += 0.001; }
+  if (rotate["z"]) { sfc_group.rotation.z += 0.001; }
+
   renderer.render( scene, camera );
 }
 render();
+
