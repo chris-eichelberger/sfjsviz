8a316d39d11a74e769b902e03850738c2e6add1a
Chris Eichelberger <cne1x@ccri.com>
2015-06-15 14:25:02 -0400

    Adding new curves.

--- data/h(2,z(2,2)).js
+++ data/h(2,z(2,2)).js
@@ -1,266 +1,267 @@
 var sfc__H2Z22 = {
 	name: "H2Z22",
-	num_dimensions:  2,
+	num_dimensions:  3,
 	bounds:  [
 		{ min: 0, max: 3 },
-		{ min: 0, max: 15 }
+		{ min: 0, max: 3 },
+		{ min: 0, max: 3 }
 	],
 	nodes:  [
 		{
 			index:  0,
-			point:  [0, 0]
+			point:  [0, 0, 0]
 		},
 		{
 			index:  1,
-			point:  [0, 1]
-		},
-		{
-			index:  14,
-			point:  [0, 2]
-		},
-		{
-			index:  15,
-			point:  [0, 3]
+			point:  [0, 0, 1]
 		},
 		{
 			index:  16,
-			point:  [0, 4]
+			point:  [0, 0, 2]
 		},
 		{
 			index:  19,
-			point:  [0, 5]
+			point:  [0, 0, 3]
+		},
+		{
+			index:  14,
+			point:  [0, 1, 0]
+		},
+		{
+			index:  15,
+			point:  [0, 1, 1]
 		},
 		{
 			index:  20,
-			point:  [0, 6]
+			point:  [0, 1, 2]
 		},
 		{
 			index:  21,
-			point:  [0, 7]
+			point:  [0, 1, 3]
 		},
 		{
 			index:  42,
-			point:  [0, 8]
+			point:  [0, 2, 0]
 		},
 		{
 			index:  43,
-			point:  [0, 9]
-		},
-		{
-			index:  44,
-			point:  [0, 10]
-		},
-		{
-			index:  47,
-			point:  [0, 11]
+			point:  [0, 2, 1]
 		},
 		{
 			index:  48,
-			point:  [0, 12]
+			point:  [0, 2, 2]
 		},
 		{
 			index:  49,
-			point:  [0, 13]
+			point:  [0, 2, 3]
+		},
+		{
+			index:  44,
+			point:  [0, 3, 0]
+		},
+		{
+			index:  47,
+			point:  [0, 3, 1]
 		},
 		{
 			index:  62,
-			point:  [0, 14]
+			point:  [0, 3, 2]
 		},
 		{
 			index:  63,
-			point:  [0, 15]
+			point:  [0, 3, 3]
 		},
 		{
 			index:  3,
-			point:  [1, 0]
+			point:  [1, 0, 0]
 		},
 		{
 			index:  2,
-			point:  [1, 1]
-		},
-		{
-			index:  13,
-			point:  [1, 2]
-		},
-		{
-			index:  12,
-			point:  [1, 3]
+			point:  [1, 0, 1]
 		},
 		{
 			index:  17,
-			point:  [1, 4]
+			point:  [1, 0, 2]
 		},
 		{
 			index:  18,
-			point:  [1, 5]
+			point:  [1, 0, 3]
+		},
+		{
+			index:  13,
+			point:  [1, 1, 0]
+		},
+		{
+			index:  12,
+			point:  [1, 1, 1]
 		},
 		{
 			index:  23,
-			point:  [1, 6]
+			point:  [1, 1, 2]
 		},
 		{
 			index:  22,
-			point:  [1, 7]
+			point:  [1, 1, 3]
 		},
 		{
 			index:  41,
-			point:  [1, 8]
+			point:  [1, 2, 0]
 		},
 		{
 			index:  40,
-			point:  [1, 9]
-		},
-		{
-			index:  45,
-			point:  [1, 10]
-		},
-		{
-			index:  46,
-			point:  [1, 11]
+			point:  [1, 2, 1]
 		},
 		{
 			index:  51,
-			point:  [1, 12]
+			point:  [1, 2, 2]
 		},
 		{
 			index:  50,
-			point:  [1, 13]
+			point:  [1, 2, 3]
+		},
+		{
+			index:  45,
+			point:  [1, 3, 0]
+		},
+		{
+			index:  46,
+			point:  [1, 3, 1]
 		},
 		{
 			index:  61,
-			point:  [1, 14]
+			point:  [1, 3, 2]
 		},
 		{
 			index:  60,
-			point:  [1, 15]
+			point:  [1, 3, 3]
 		},
 		{
 			index:  4,
-			point:  [2, 0]
+			point:  [2, 0, 0]
 		},
 		{
 			index:  7,
-			point:  [2, 1]
-		},
-		{
-			index:  8,
-			point:  [2, 2]
-		},
-		{
-			index:  11,
-			point:  [2, 3]
+			point:  [2, 0, 1]
 		},
 		{
 			index:  30,
-			point:  [2, 4]
+			point:  [2, 0, 2]
 		},
 		{
 			index:  29,
-			point:  [2, 5]
+			point:  [2, 0, 3]
+		},
+		{
+			index:  8,
+			point:  [2, 1, 0]
+		},
+		{
+			index:  11,
+			point:  [2, 1, 1]
 		},
 		{
 			index:  24,
-			point:  [2, 6]
+			point:  [2, 1, 2]
 		},
 		{
 			index:  25,
-			point:  [2, 7]
+			point:  [2, 1, 3]
 		},
 		{
 			index:  38,
-			point:  [2, 8]
+			point:  [2, 2, 0]
 		},
 		{
 			index:  39,
-			point:  [2, 9]
-		},
-		{
-			index:  34,
-			point:  [2, 10]
-		},
-		{
-			index:  33,
-			point:  [2, 11]
+			point:  [2, 2, 1]
 		},
 		{
 			index:  52,
-			point:  [2, 12]
+			point:  [2, 2, 2]
 		},
 		{
 			index:  55,
-			point:  [2, 13]
+			point:  [2, 2, 3]
+		},
+		{
+			index:  34,
+			point:  [2, 3, 0]
+		},
+		{
+			index:  33,
+			point:  [2, 3, 1]
 		},
 		{
 			index:  56,
-			point:  [2, 14]
+			point:  [2, 3, 2]
 		},
 		{
 			index:  59,
-			point:  [2, 15]
+			point:  [2, 3, 3]
 		},
 		{
 			index:  5,
-			point:  [3, 0]
+			point:  [3, 0, 0]
 		},
 		{
 			index:  6,
-			point:  [3, 1]
-		},
-		{
-			index:  9,
-			point:  [3, 2]
-		},
-		{
-			index:  10,
-			point:  [3, 3]
+			point:  [3, 0, 1]
 		},
 		{
 			index:  31,
-			point:  [3, 4]
+			point:  [3, 0, 2]
 		},
 		{
 			index:  28,
-			point:  [3, 5]
+			point:  [3, 0, 3]
+		},
+		{
+			index:  9,
+			point:  [3, 1, 0]
+		},
+		{
+			index:  10,
+			point:  [3, 1, 1]
 		},
 		{
 			index:  27,
-			point:  [3, 6]
+			point:  [3, 1, 2]
 		},
 		{
 			index:  26,
-			point:  [3, 7]
+			point:  [3, 1, 3]
 		},
 		{
 			index:  37,
-			point:  [3, 8]
+			point:  [3, 2, 0]
 		},
 		{
 			index:  36,
-			point:  [3, 9]
-		},
-		{
-			index:  35,
-			point:  [3, 10]
-		},
-		{
-			index:  32,
-			point:  [3, 11]
+			point:  [3, 2, 1]
 		},
 		{
 			index:  53,
-			point:  [3, 12]
+			point:  [3, 2, 2]
 		},
 		{
 			index:  54,
-			point:  [3, 13]
+			point:  [3, 2, 3]
+		},
+		{
+			index:  35,
+			point:  [3, 3, 0]
+		},
+		{
+			index:  32,
+			point:  [3, 3, 1]
 		},
 		{
 			index:  57,
-			point:  [3, 14]
+			point:  [3, 3, 2]
 		},
 		{
 			index:  58,
-			point:  [3, 15]
+			point:  [3, 3, 3]
 		},
 	]
 };
--- data/r(2,z(2,2)).js
+++ data/r(2,z(2,2)).js
@@ -1,266 +1,267 @@
 var sfc__R2Z22 = {
 	name: "R2Z22",
-	num_dimensions:  2,
+	num_dimensions:  3,
 	bounds:  [
 		{ min: 0, max: 3 },
-		{ min: 0, max: 15 }
+		{ min: 0, max: 3 },
+		{ min: 0, max: 3 }
 	],
 	nodes:  [
 		{
 			index:  0,
-			point:  [0, 0]
+			point:  [0, 0, 0]
 		},
 		{
 			index:  1,
-			point:  [0, 1]
-		},
-		{
-			index:  2,
-			point:  [0, 2]
-		},
-		{
-			index:  3,
-			point:  [0, 3]
+			point:  [0, 0, 1]
 		},
 		{
 			index:  4,
-			point:  [0, 4]
+			point:  [0, 0, 2]
 		},
 		{
 			index:  5,
-			point:  [0, 5]
+			point:  [0, 0, 3]
+		},
+		{
+			index:  2,
+			point:  [0, 1, 0]
+		},
+		{
+			index:  3,
+			point:  [0, 1, 1]
 		},
 		{
 			index:  6,
-			point:  [0, 6]
+			point:  [0, 1, 2]
 		},
 		{
 			index:  7,
-			point:  [0, 7]
+			point:  [0, 1, 3]
 		},
 		{
 			index:  8,
-			point:  [0, 8]
+			point:  [0, 2, 0]
 		},
 		{
 			index:  9,
-			point:  [0, 9]
-		},
-		{
-			index:  10,
-			point:  [0, 10]
-		},
-		{
-			index:  11,
-			point:  [0, 11]
+			point:  [0, 2, 1]
 		},
 		{
 			index:  12,
-			point:  [0, 12]
+			point:  [0, 2, 2]
 		},
 		{
 			index:  13,
-			point:  [0, 13]
+			point:  [0, 2, 3]
+		},
+		{
+			index:  10,
+			point:  [0, 3, 0]
+		},
+		{
+			index:  11,
+			point:  [0, 3, 1]
 		},
 		{
 			index:  14,
-			point:  [0, 14]
+			point:  [0, 3, 2]
 		},
 		{
 			index:  15,
-			point:  [0, 15]
+			point:  [0, 3, 3]
 		},
 		{
 			index:  16,
-			point:  [1, 0]
+			point:  [1, 0, 0]
 		},
 		{
 			index:  17,
-			point:  [1, 1]
-		},
-		{
-			index:  18,
-			point:  [1, 2]
-		},
-		{
-			index:  19,
-			point:  [1, 3]
+			point:  [1, 0, 1]
 		},
 		{
 			index:  20,
-			point:  [1, 4]
+			point:  [1, 0, 2]
 		},
 		{
 			index:  21,
-			point:  [1, 5]
+			point:  [1, 0, 3]
+		},
+		{
+			index:  18,
+			point:  [1, 1, 0]
+		},
+		{
+			index:  19,
+			point:  [1, 1, 1]
 		},
 		{
 			index:  22,
-			point:  [1, 6]
+			point:  [1, 1, 2]
 		},
 		{
 			index:  23,
-			point:  [1, 7]
+			point:  [1, 1, 3]
 		},
 		{
 			index:  24,
-			point:  [1, 8]
+			point:  [1, 2, 0]
 		},
 		{
 			index:  25,
-			point:  [1, 9]
-		},
-		{
-			index:  26,
-			point:  [1, 10]
-		},
-		{
-			index:  27,
-			point:  [1, 11]
+			point:  [1, 2, 1]
 		},
 		{
 			index:  28,
-			point:  [1, 12]
+			point:  [1, 2, 2]
 		},
 		{
 			index:  29,
-			point:  [1, 13]
+			point:  [1, 2, 3]
+		},
+		{
+			index:  26,
+			point:  [1, 3, 0]
+		},
+		{
+			index:  27,
+			point:  [1, 3, 1]
 		},
 		{
 			index:  30,
-			point:  [1, 14]
+			point:  [1, 3, 2]
 		},
 		{
 			index:  31,
-			point:  [1, 15]
+			point:  [1, 3, 3]
 		},
 		{
 			index:  32,
-			point:  [2, 0]
+			point:  [2, 0, 0]
 		},
 		{
 			index:  33,
-			point:  [2, 1]
-		},
-		{
-			index:  34,
-			point:  [2, 2]
-		},
-		{
-			index:  35,
-			point:  [2, 3]
+			point:  [2, 0, 1]
 		},
 		{
 			index:  36,
-			point:  [2, 4]
+			point:  [2, 0, 2]
 		},
 		{
 			index:  37,
-			point:  [2, 5]
+			point:  [2, 0, 3]
+		},
+		{
+			index:  34,
+			point:  [2, 1, 0]
+		},
+		{
+			index:  35,
+			point:  [2, 1, 1]
 		},
 		{
 			index:  38,
-			point:  [2, 6]
+			point:  [2, 1, 2]
 		},
 		{
 			index:  39,
-			point:  [2, 7]
+			point:  [2, 1, 3]
 		},
 		{
 			index:  40,
-			point:  [2, 8]
+			point:  [2, 2, 0]
 		},
 		{
 			index:  41,
-			point:  [2, 9]
-		},
-		{
-			index:  42,
-			point:  [2, 10]
-		},
-		{
-			index:  43,
-			point:  [2, 11]
+			point:  [2, 2, 1]
 		},
 		{
 			index:  44,
-			point:  [2, 12]
+			point:  [2, 2, 2]
 		},
 		{
 			index:  45,
-			point:  [2, 13]
+			point:  [2, 2, 3]
+		},
+		{
+			index:  42,
+			point:  [2, 3, 0]
+		},
+		{
+			index:  43,
+			point:  [2, 3, 1]
 		},
 		{
 			index:  46,
-			point:  [2, 14]
+			point:  [2, 3, 2]
 		},
 		{
 			index:  47,
-			point:  [2, 15]
+			point:  [2, 3, 3]
 		},
 		{
 			index:  48,
-			point:  [3, 0]
+			point:  [3, 0, 0]
 		},
 		{
 			index:  49,
-			point:  [3, 1]
-		},
-		{
-			index:  50,
-			point:  [3, 2]
-		},
-		{
-			index:  51,
-			point:  [3, 3]
+			point:  [3, 0, 1]
 		},
 		{
 			index:  52,
-			point:  [3, 4]
+			point:  [3, 0, 2]
 		},
 		{
 			index:  53,
-			point:  [3, 5]
+			point:  [3, 0, 3]
+		},
+		{
+			index:  50,
+			point:  [3, 1, 0]
+		},
+		{
+			index:  51,
+			point:  [3, 1, 1]
 		},
 		{
 			index:  54,
-			point:  [3, 6]
+			point:  [3, 1, 2]
 		},
 		{
 			index:  55,
-			point:  [3, 7]
+			point:  [3, 1, 3]
 		},
 		{
 			index:  56,
-			point:  [3, 8]
+			point:  [3, 2, 0]
 		},
 		{
 			index:  57,
-			point:  [3, 9]
-		},
-		{
-			index:  58,
-			point:  [3, 10]
-		},
-		{
-			index:  59,
-			point:  [3, 11]
+			point:  [3, 2, 1]
 		},
 		{
 			index:  60,
-			point:  [3, 12]
+			point:  [3, 2, 2]
 		},
 		{
 			index:  61,
-			point:  [3, 13]
+			point:  [3, 2, 3]
+		},
+		{
+			index:  58,
+			point:  [3, 3, 0]
+		},
+		{
+			index:  59,
+			point:  [3, 3, 1]
 		},
 		{
 			index:  62,
-			point:  [3, 14]
+			point:  [3, 3, 2]
 		},
 		{
 			index:  63,
-			point:  [3, 15]
+			point:  [3, 3, 3]
 		},
 	]
 };
