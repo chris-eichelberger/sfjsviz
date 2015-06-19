53c1349a795659863b425303f108d5734f5a0f04
Chris Eichelberger <cne1x@ccri.com>
2015-06-19 10:37:56 -0400

    Allows for curve-selection in the URL.

---
+++ data/Z222.js
@@ -0,0 +1,267 @@
+var sfc__Z222 = {
+	name: "Z222",
+	num_dimensions:  3,
+	bounds:  [
+		{ min: 0, max: 3 },
+		{ min: 0, max: 3 },
+		{ min: 0, max: 3 }
+	],
+	nodes:  [
+		{
+			index:  0,
+			point:  [0, 0, 0]
+		},
+		{
+			index:  1,
+			point:  [0, 0, 1]
+		},
+		{
+			index:  8,
+			point:  [0, 0, 2]
+		},
+		{
+			index:  9,
+			point:  [0, 0, 3]
+		},
+		{
+			index:  2,
+			point:  [0, 1, 0]
+		},
+		{
+			index:  3,
+			point:  [0, 1, 1]
+		},
+		{
+			index:  10,
+			point:  [0, 1, 2]
+		},
+		{
+			index:  11,
+			point:  [0, 1, 3]
+		},
+		{
+			index:  16,
+			point:  [0, 2, 0]
+		},
+		{
+			index:  17,
+			point:  [0, 2, 1]
+		},
+		{
+			index:  24,
+			point:  [0, 2, 2]
+		},
+		{
+			index:  25,
+			point:  [0, 2, 3]
+		},
+		{
+			index:  18,
+			point:  [0, 3, 0]
+		},
+		{
+			index:  19,
+			point:  [0, 3, 1]
+		},
+		{
+			index:  26,
+			point:  [0, 3, 2]
+		},
+		{
+			index:  27,
+			point:  [0, 3, 3]
+		},
+		{
+			index:  4,
+			point:  [1, 0, 0]
+		},
+		{
+			index:  5,
+			point:  [1, 0, 1]
+		},
+		{
+			index:  12,
+			point:  [1, 0, 2]
+		},
+		{
+			index:  13,
+			point:  [1, 0, 3]
+		},
+		{
+			index:  6,
+			point:  [1, 1, 0]
+		},
+		{
+			index:  7,
+			point:  [1, 1, 1]
+		},
+		{
+			index:  14,
+			point:  [1, 1, 2]
+		},
+		{
+			index:  15,
+			point:  [1, 1, 3]
+		},
+		{
+			index:  20,
+			point:  [1, 2, 0]
+		},
+		{
+			index:  21,
+			point:  [1, 2, 1]
+		},
+		{
+			index:  28,
+			point:  [1, 2, 2]
+		},
+		{
+			index:  29,
+			point:  [1, 2, 3]
+		},
+		{
+			index:  22,
+			point:  [1, 3, 0]
+		},
+		{
+			index:  23,
+			point:  [1, 3, 1]
+		},
+		{
+			index:  30,
+			point:  [1, 3, 2]
+		},
+		{
+			index:  31,
+			point:  [1, 3, 3]
+		},
+		{
+			index:  32,
+			point:  [2, 0, 0]
+		},
+		{
+			index:  33,
+			point:  [2, 0, 1]
+		},
+		{
+			index:  40,
+			point:  [2, 0, 2]
+		},
+		{
+			index:  41,
+			point:  [2, 0, 3]
+		},
+		{
+			index:  34,
+			point:  [2, 1, 0]
+		},
+		{
+			index:  35,
+			point:  [2, 1, 1]
+		},
+		{
+			index:  42,
+			point:  [2, 1, 2]
+		},
+		{
+			index:  43,
+			point:  [2, 1, 3]
+		},
+		{
+			index:  48,
+			point:  [2, 2, 0]
+		},
+		{
+			index:  49,
+			point:  [2, 2, 1]
+		},
+		{
+			index:  56,
+			point:  [2, 2, 2]
+		},
+		{
+			index:  57,
+			point:  [2, 2, 3]
+		},
+		{
+			index:  50,
+			point:  [2, 3, 0]
+		},
+		{
+			index:  51,
+			point:  [2, 3, 1]
+		},
+		{
+			index:  58,
+			point:  [2, 3, 2]
+		},
+		{
+			index:  59,
+			point:  [2, 3, 3]
+		},
+		{
+			index:  36,
+			point:  [3, 0, 0]
+		},
+		{
+			index:  37,
+			point:  [3, 0, 1]
+		},
+		{
+			index:  44,
+			point:  [3, 0, 2]
+		},
+		{
+			index:  45,
+			point:  [3, 0, 3]
+		},
+		{
+			index:  38,
+			point:  [3, 1, 0]
+		},
+		{
+			index:  39,
+			point:  [3, 1, 1]
+		},
+		{
+			index:  46,
+			point:  [3, 1, 2]
+		},
+		{
+			index:  47,
+			point:  [3, 1, 3]
+		},
+		{
+			index:  52,
+			point:  [3, 2, 0]
+		},
+		{
+			index:  53,
+			point:  [3, 2, 1]
+		},
+		{
+			index:  60,
+			point:  [3, 2, 2]
+		},
+		{
+			index:  61,
+			point:  [3, 2, 3]
+		},
+		{
+			index:  54,
+			point:  [3, 3, 0]
+		},
+		{
+			index:  55,
+			point:  [3, 3, 1]
+		},
+		{
+			index:  62,
+			point:  [3, 3, 2]
+		},
+		{
+			index:  63,
+			point:  [3, 3, 3]
+		},
+	]
+};
